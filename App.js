1e0c644f6e21f2d370fc0b1b6d66e04d62c55db3
Cailewey <cwymxs26@gmail.com>
2026-04-01 02:41:59 -0600

    Import LoginScreen and remove RegisterScreen

--- App.js
+++ App.js
@@ -1,19 +1,2 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <RegisterScreen />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1a1a1a', // Color oscuro para combinar con Cailewey
-  },
-});
-
